3b2ce38e35349efdab5f1354e4dbb67a19f439ac
Ananth Kamath <ananthkamath98@gmail.com>
2025-06-10 22:31:20 -0700

    Full pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is my frist time setting up a continuous delivery pipeline'));
+app.get('/', (req, res) => res.send('Hello! This is the latest message'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
